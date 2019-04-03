5fcf9f2ee49e26859ba324a6a6d2870df44ba80b
HJQ <543796673@qq.com>
2019-04-03 23:59:00 +0800

    solve the router with the navmenu, add css/dashboard.css
    redirect、created method knowledge

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,19 +1,51 @@
-// var  test = {
-//     template: '#overview',
-// }
 
-// var  routtest = new VueRouter({
-//     routes: [
-//     	//path 表示监听 路由地址
-//     	//component 如果是path的路由地址 表示展示所对应的组件
-//         { path: '/test', component: test },
-//     ]
-// })
+var overview = {
+    template: '#overview',
+    //    created(){//生命周期钩子函数
+    //    	console.log(this.$route.path);
+    //    }
+}
+var problem = {
+    template: '#problem',
+}
+var statistics = {
+    template: '#statistics',
+}
+var about = {
+    template: '#about',
+}
 
+var router = new VueRouter({
+    routes: [
+        // path 表示监听路由地址
+        // component组件模板-> 如果path是正确的路由地址，那就就展示路径所对应的组件模板样式
+        // 这里使用了重定向redirect
+        {
+        path: '/',
+        redirect: '/overview',
+        },
+        {
+            path: '/overview',
+            component: overview,
+        },
+        {
+            path: '/problem',
+            component: problem
+        }, {
+            path: '/statistics',
+            component: statistics
+        },
+        {
+            path: '/about',
+            component: about
+        },
+    ]
+})
 
 new Vue({
     el: "#app",
     methods: {},
     data: {},
-   // router: routtest,
+    router: router,
+
 })
